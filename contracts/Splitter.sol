06f7fa8ffaeab76c6e81dced50356aad02adda70
n_44o <nfurfaro33@gmail.com>
2017-11-14 10:25:55 -0700

    refactoring. still have hard-coded address!

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,74 +3,91 @@ pragma solidity ^0.4.6;
 
 contract Splitter {
     address public owner;
-    uint public contractBalance;
+    bool public frozen;
     address public Bob;
     address public Carol;
     uint public onePortion;
-    uint public remainder;
     uint public transferAmount;
 
     event LogDeposit(address depositor, uint amount);
     event LogWithdrawl(address withdrawer, uint amount);
-    event LogContractState(uint timestamp, address splitter, uint balance, uint bobsFunds, uint carolsFunds);
+    event LogContractState(uint balance, uint bobsFunds, uint carolsFunds);
 
-    struct chosenOne {
-        uint availableFunds;
-    }
-
-    mapping(address => chosenOne) public theChosenOnes;
+    mapping(address => uint) public theChosenOnes;
     
-
     function Splitter() public {
         owner = msg.sender;
         // Bob = _Bob;
         // Carol = _Carol;
-        Bob = 0x97314660e157102ddd219f16c80005c4c03ce659; 
+        Bob = 0x97314660e157102ddd219f16c80005c4c03ce659;
         Carol = 0x156ee3356777139a2bbe695a4b1ccd5b692ca60c;
-        // contractBalance = this.balance;
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _; 
+    }
+
+    modifier freezeRay() {
+        require(frozen == false);
+        _;
     }
     
     function deposit()
+        freezeRay
+        isOwner
         public 
         payable                            
-        returns(bool successfulSplit) {   
-            require(msg.sender == owner);
-            require(msg.value != 0);
-            LogDeposit(msg.sender, msg.value);
-            LogContractState(block.timestamp, this, this.balance, theChosenOnes[Bob].availableFunds, theChosenOnes[Carol].availableFunds);
-            contractBalance = this.balance;
-            remainder += msg.value % 2;
-            onePortion = msg.value / 2;
-            theChosenOnes[Bob].availableFunds += onePortion;
-            theChosenOnes[Carol].availableFunds += onePortion;
-            LogContractState(block.timestamp, this, this.balance, theChosenOnes[Bob].availableFunds, theChosenOnes[Carol].availableFunds);
-            return true;
-        }
-
-    function withdrawFunds() 
-        public
-        returns(bool SuccessfulWithdrawl) 
-    {
-        require(theChosenOnes[msg.sender].availableFunds != 0);
-        transferAmount = theChosenOnes[msg.sender].availableFunds + remainder; 
-        theChosenOnes[msg.sender].availableFunds = 0;
-        remainder = 0;
-        msg.sender.transfer(transferAmount);
-        contractBalance = this.balance;
-        LogWithdrawl(msg.sender, transferAmount);
-        LogContractState(block.timestamp, this, this.balance, theChosenOnes[Bob].availableFunds, theChosenOnes[Carol].availableFunds);
-        return true;
+    {  
+        require(msg.value != 0);
+        LogDeposit(msg.sender, msg.value);
+        LogContractState(this.balance, theChosenOnes[Bob], theChosenOnes[Carol]);
+        uint remainder = msg.value % 2;
+        // uint value = msg.value;
+        onePortion = msg.value / 2;
+        theChosenOnes[Bob] += onePortion;
+        theChosenOnes[Carol] += onePortion + remainder;
+        LogContractState(this.balance, theChosenOnes[Bob], theChosenOnes[Carol]);
+        onePortion = 0;
     }
+
+    function withdrawFunds()
+        freezeRay 
+        public 
+    {
+        require(frozen == false);
+        require(theChosenOnes[msg.sender] != 0);
+            LogContractState(this.balance, theChosenOnes[Bob], theChosenOnes[Carol]);
+            transferAmount = theChosenOnes[msg.sender]; 
+            theChosenOnes[msg.sender] = 0;
+            msg.sender.transfer(transferAmount);
+            LogWithdrawl(msg.sender, transferAmount);
+            LogContractState(this.balance, theChosenOnes[Bob], theChosenOnes[Carol]); 
+    } 
     
-    function getBalanceA() public constant returns(uint) {
+    function getContractBalance() freezeRay public constant returns(uint) {
+            return this.balance;
+    }
+
+    function getBalanceA() freezeRay public constant returns(uint) {
+        require(frozen == false);
         return owner.balance;
     }
     
-    function getBalanceB() public constant returns(uint) {
+    function getBalanceB() freezeRay public constant returns(uint) {
+        require(frozen == false);
         return Bob.balance;    
     }
     
-    function getBalanceC() public constant returns(uint) {
+    function getBalanceC() freezeRay public constant returns(uint) {
+        require(frozen == false);
         return Carol.balance;    
+    }
+
+    function freeze(bool _freeze)
+        isOwner
+        returns (bool success) {
+            frozen = _freeze;
+            return true;
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -1,7 +1,12 @@
-var Splitter = artifacts.require("./Splitter.sol");
+const Splitter = artifacts.require("./Splitter.sol");
+const chai = require('chai');
+const assert = chai.assert;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 contract('Splitter', accounts => {
   let instance;
+  let eventObject;
   let owner = accounts[0] ;
   let Bob = accounts[1];
   let Carol = accounts[2];
@@ -12,102 +17,94 @@ contract('Splitter', accounts => {
   let txFee;
   let payout;
 
-  beforeEach(function() {
+  beforeEach(() => {
     return Splitter.new({ from: owner }).then(_instance => {
-      instance = _instance;	
+      instance = _instance;
     })
   });
 
-  it("should be owned by owner", () => {
-  	return instance.owner({from: owner}).then(_owner => {
-      assert.strictEqual(_owner, owner, "Contract is not owned by owner");
+  it("should be owned by owner", function() {
+  	  return assert.eventually.strictEqual(Promise.resolve(instance.owner({from: owner})), owner, "Contract is not owned by owner");
+
+  });
+
+  it("should add a new deposit to the contract balance", function() {
+  	  return instance.deposit({from: owner, value: 10}).then(function(txObj) {
+  	      return instance.getContractBalance.call()
+  	  }).then(function(_balance) {
+  	      assert.equal(_balance.toString(10), "10", "Splitter balance was not increased by 10 wei")
   	})
   });
 
-  it("should start with a balance of 0", () => {
-  	return instance.contractBalance().then(_balance => {
-  	  assert.equal(_balance.toString(10), "0", "Splitter was not initialized with a balance of 0");
-  	});
-  });
-
-  it("should add a new deposit to the contract balance", () => {
-  	return instance.deposit({from: owner, value: 10}).then((txObj) => {
-  	  return instance.contractBalance()
-  	}).then((_balance) => {
-  	  assert.equal(_balance.toString(10), "10", "Splitter balance was not increased by 10 wei")
-  	})
-  });
-
-  it("should keep track of the total value of all deposits", () => {
-  	return instance.deposit({from: owner, value: 2}).then (() => {
-  	  return instance.deposit({from: owner, value: 14})
-  	}).then((txObj) => {
-  	  return instance.deposit({from: owner, value: 14})
-  	}).then ((txObj) => {
-  	  return instance.contractBalance()
-  	}).then(_balance => {
-  	  assert.equal(_balance.toString(10), "30", "Splitter balance was not properly credited")
+  it("should keep track of the total value of all deposits", function() {
+  	return instance.deposit({from: owner, value: 2}).then (function() {
+  	    return instance.deposit({from: owner, value: 14})
+  	}).then(function(txObj) {
+  		// console.log(txObj.receipt.logs);
+  	    return instance.deposit({from: owner, value: 14})
+  	}).then (function() {
+  	    return instance.getContractBalance.call()
+  	}).then(function(_balance) {
+  	    assert.equal(_balance.toString(10), "30", "Splitter balance was not properly credited")
   	})
   })
 
-  it("should allow Bob and Carol to each withdraw half of the funds", () => {
-  	initialBalance = web3.toBigNumber(web3.eth.getBalance(Bob).toString(10));
-  	return instance.deposit({from: owner, value: 30}
-  	).then((txObj) => {	
-  	  return instance.withdrawFunds({from: Bob})
-  	}).then((txObj) => {
-  	  payout = txObj.logs[0].args.amount.toString(10);
-  	  currentBalance = web3.toBigNumber(web3.eth.getBalance(Bob));
-  	  console.log("payout: " + payout);
-  	  console.log("initialBalance: " + initialBalance.toString(10));
-  	  console.log("currentBalance: " + currentBalance.toString(10));
-  	  console.log("difference: " + currentBalance.minus(initialBalance).toString(10));
-  	  gasUsed = txObj.receipt.gasUsed;
-  	  console.log("gasUsed: " +gasUsed);
-  	  gasPrice = web3.eth.gasPrice;
-  	  console.log("gasPrice: " + gasPrice.toString(10));
-  	  txFee = gasPrice.times(web3.toBigNumber(gasUsed));
-  	   console.log("txFee: " + txFee.toString(10)); 
-  	  assert.equal(txObj.logs.length, 2, "should have received 2 events");
-  	  assert.equal(txObj.logs[0].args.amount.toString(10), "15", "Bob's share wasn't properly allocated");
-  	  // assert.equal(initialBalance.plus(payout).minus(txFee).toString(10), currentBalance.toString(10), "Bob failed to receive the correct ammount of ether in to his account")
 
-      assert.equal(currentBalance.minus(payout).minus(txFee).toString(10), initialBalance.toString(10), "Bob failed to receive the correct ammount of ether in to his account")
-  	}).then(() => {
-  		return instance.contractBalance()
-  	}).then(_balance => {
-  	  assert.equal(_balance.toString(10), "15", "Splitter balance was not properly debited")
-  	}).then(() => {
-  	  return instance.withdrawFunds({from: Carol})
-  	}).then((txObj) => {
-  	  assert.equal(txObj.logs[0].args.amount.toString(10), "15", "Carol's share wasn't properly allocated");
-  	  return instance.contractBalance()
-  	}).then(_balance => {
-  	  assert.equal(_balance.toString(10), "0", "Splitter balance was not properly debited")
-  	})
-  })
+//check that he actually got it
+  it("should allow Bob to withdraw 25 wei", function() {
+  	  return instance.deposit({from: owner, value: 50}).then(function() {
+  	      return instance.getContractBalance.call()
+  	  }).then(function(_balance) {
+          return instance.withdrawFunds({from: Bob})
+      }).then(function(txObj) {
+      	  // console.log(txObj.receipt.logs);
+          assert.equal(txObj.logs[0].args.bobsFunds.toString(10), "25", "Bob's share wasn't properly allocated");
+      });
+   });
 
-  it("should not allow Bob to withdraw more than his share", () => {
-  	return instance.contractBalance().then(_balance => {
-  	  assert.equal(_balance.toString(10), "0", "The starting balance is incorrect here.")
-  	}).then (() => {
-  	  return instance.deposit({from: owner, value: 50})
-  	}).then((txObj) => {
-  	  return instance.withdrawFunds({from: Bob})
-  	}).then (() => {
-  	  return instance.deposit({from: owner, value: 10})
-  	}).then((txObj) => {
-  	  return instance.withdrawFunds({from: Bob})
-  	}).then((txObj) => {
-  	  return instance.contractBalance()
-  	}).then(_balance => {
-  	  assert.equal(_balance.toString(10), "30", "Splitter balance was not properly debited")
-  	}).then(() => {
-  	  return instance.withdrawFunds({from: Carol})
-  	}).then((txObj) => {
-  	  return instance.contractBalance()
-  	}).then(_balance => {
-  	  assert.equal(_balance.toString(10), "0", "Splitter balance was not properly debited")
-  	})
-  })
+   //check that she actually got it
+  it("should allow Carol to withdraw 11 wei", function() {
+  	  return instance.deposit({from: owner, value: 22}).then(function() {
+  	      return instance.getContractBalance.call()
+  	  }).then(function(_balance) {
+          return instance.withdrawFunds({from: Carol})
+      }).then(function(txObj) {
+      	  // console.log(txObj.receipt.logs);
+          assert.equal(txObj.logs[0].args.carolsFunds.toString(10), "11", "Carols's share wasn't properly allocated");
+      });
+   });
+
+  // Xavier:
+// "You should not use the sync' getBalance.
+// The difference is Bob's tx fee. You need to get that. The withdraw tx.
+// And gasPrice has to come from your tx.""
+
+   it("should confirm that carol actually received her funds", () => {
+   		// initialBalance = web3.toBigNumber(Carol.balance);
+   		initialBalance = web3.toBigNumber(web3.eth.getBalance(Carol));
+        return instance.deposit({from: owner, value: 21}).then(() => {  
+            return instance.withdrawFunds({from: Carol})
+        }).then((txObj) => {
+            payout = txObj.logs[0].args.carolsFunds.toString(10);
+            currentBalance = web3.toBigNumber(web3.eth.getBalance(Carol));
+            console.log("payout: " + payout);
+            console.log("initialBalance: " + initialBalance.toString(10));
+            console.log("currentBalance: " + currentBalance.toString(10));
+            console.log("difference: " + currentBalance.minus(initialBalance).toString(10));
+            gasUsed = txObj.receipt.gasUsed;
+            console.log("gasUsed: " + gasUsed);
+            gasPrice = web3.eth.gasPrice;
+            web3.eth.getTransaction(txObj.tx, () => {
+                gasPrice = txObj.tx.gasPrice;
+            });
+            console.log("gasPrice: " + gasPrice);
+            txFee = gasPrice.times(web3.toBigNumber(gasUsed));
+            console.log("txFee: " + txFee.toString(10)); 
+            assert.equal(initialBalance.plus(payout).minus(txFee).toString(10), currentBalance.toString(10), "Carol didn't get her ether")
+        })
+    });
+
 });
+
+
+  
